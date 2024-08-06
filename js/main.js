69dc77d10172dd97bdf0f63762ccc9f2f25f7b19
Mark Skelton <markskelton10@gmail.com>
2024-08-06 15:17:58 +0100

    Create grid of squares out of div elements
    At the moment it is 16x16. Used JS to help with recreating the grid
    later.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 "use strict";
 
-console.log("Hello World");
+const container = document.querySelector(".container");
+const squaresPerSide = 16;
+
+for (let i=0; i<squaresPerSide; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let j=0; j<squaresPerSide; j++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        row.appendChild(square);
+    }
+    container.appendChild(row);
+}
