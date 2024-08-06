63a3379f29d08e4852814f65e12261c3204927fb
Mark Skelton <markskelton10@gmail.com>
2024-08-06 16:04:03 +0100

    Add create new grid button
    This sets the number of squares per side for the new grid from user
    input and creates a new grid. Moved grid creation code into a function
    to be reused. Limited input to 100 for performance.

--- js/main.js
+++ js/main.js
@@ -2,20 +2,39 @@
 
 const container = document.querySelector(".container");
 container.addEventListener("mouseover", colorIn);
-const squaresPerSide = 16;
 
-for (let i=0; i<squaresPerSide; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    for (let j=0; j<squaresPerSide; j++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        row.appendChild(square);
+const newGridBtn = document.querySelector(".btn-new-grid");
+newGridBtn.addEventListener("click", getUserInput);
+
+createGrid(16);
+
+function createGrid(squaresPerSide) {
+    for (let i=0; i<squaresPerSide; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for (let j=0; j<squaresPerSide; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            row.appendChild(square);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
 
 function colorIn(event) {
     const square = event.target;
     square.style.backgroundColor = "black";
+}
+
+function getUserInput() {
+    const squaresPerSide = +prompt(
+        "Please enter the integer number of squares for the new grid from 1 to 100", "16"
+    );
+    if (!Number.isInteger(squaresPerSide) || squaresPerSide < 1 || squaresPerSide > 100) {
+        alert("Abort: Input must be an integer between 1 and 100");
+        return;
+    }
+    
+    container.innerHTML = "";
+    createGrid(squaresPerSide);
 }
